fd2ee712cd114486e7147c0f8f84149eee02378b
Suporte <Suporte@192.168.25.146>
2018-09-01 13:03:37 -0300

    Semestre 2018.1

--- app_gabarito/src/br/util/RetornaDiretorio.java
+++ app_gabarito/src/br/util/RetornaDiretorio.java
@@ -2,7 +2,7 @@ package br.util;
 
 public class RetornaDiretorio {
 	
-	public static final String SEMESTRE = "2018.1";
+	public static final String SEMESTRE = "2018.2";
 	
 	public String getUrlWebContent(){
 		String dir = System.getProperty("user.dir")+"\\WebContent\\";
